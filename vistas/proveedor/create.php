7c06e324fd378936b21330e84ee6037c4c09e6f5
Ezekl22 <leonardo22eze@gmail.com>
2023-07-24 00:11:19 -0300

    feature/proveedores
    termino la parte de proveedores

--- vistas/proveedor/create.php
+++ vistas/proveedor/create.php
@@ -13,21 +13,27 @@
         <article class="mt-5 d-flex flex-column align-items-center">
             <div class="grilla w-75 d-flex flex-column align-items-center rounded-4">
                 <form action="" method="POST" class="w-100 d-flex align-items-center flex-column">
-                    <div class="border w-75 mt-5 mb-2 rounded-4 d-flex flex-column align-items-center">
-                        <div class="w-75 d-flex mt-3">
+                    <div class="border w-95 mt-5 mb-2 rounded-4 d-flex flex-column align-items-center">
+                        <div class="w-75 d-flex mt-4 mb-3">
                             <div class="input-group input-group-sm mb-3">
                                 <span class="input-group-text" id="inputGroup-sizing-sm">Nombre</span>
                                 <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" id="nombre" name="nombre" required>
                             </div>
-                            <div class="input-group input-group-sm mb-3 ms-4">
-                                <span class="input-group-text" id="inputGroup-sizing-sm">Categoria Fiscal</span>
-                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" id="categoria_fiscal" name="categoria_fiscal" required>
+                            <div class="input-group input-group-sm mb-2 ms-5">
+                                <label class="input-group-text" for="tipo">Categoria Fiscal:</label>
+                                <select class="form-select" id="tipo" name="tipo" required>
+                                    <option value="Monotributista">Monotributista</option>
+                                    <option value="Responsable inscripto">Responsable inscripto</option>
+                                    <option value="Excento">Excento</option>
+                                </select>
                             </div>
                             <div class="input-group input-group-sm mb-3 ms-4">
                                 <span class="input-group-text" id="inputGroup-sizing-sm">Direccion</span>
                                 <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" id="direccion" name="direccion" required>
                             </div>
-                            <div class="input-group input-group-sm mb-3 ms-4">
+                        </div>
+                        <div class="w-75 d-flex my-3">
+                            <div class="input-group input-group-sm mb-3">
                                 <span class="input-group-text" id="inputGroup-sizing-sm">Telefono</span>
                                 <input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" id="telefono" name="telefono" required>
                             </div>
