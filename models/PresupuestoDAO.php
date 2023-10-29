bcba0410a770c0437fc2e7d4934be6633277a97f
Ezekl22 <leonardo22eze@gmail.com>
2023-10-29 18:28:30 -0300

    feature/presupuesto
    hago el metodo create en el presupuestoDAO

--- models/PresupuestoDAO.php
+++ models/PresupuestoDAO.php
@@ -10,19 +10,34 @@ class PresupuestoDAO {
     }
 
     public function create(PresupuestoMdl $presupuesto) {
-        $stmt = $this->db->getConnection()->prepare("INSERT INTO users (name, lastname, mail, password, type) VALUES (:name, :lastname, :mail, :password, :type)");
-        
-        $name = $user->getName();
-        $lastname = $user->getLastname();
-        $mail = $user->getMail();
-        $password = $user->getPassword();
-        $type = $user->getType();
-        
-		$stmt->bindParam(":name", $name, PDO::PARAM_STR);
-		$stmt->bindParam(":lastname", $lastname, PDO::PARAM_STR);
-        $stmt->bindParam(":mail", $mail, PDO::PARAM_STR);
-		$stmt->bindParam(":password", $password, PDO::PARAM_STR);
-		$stmt->bindParam(":type", $type, PDO::PARAM_STR);
+        $productos = $presupuesto->getProductos();
+        $productosValues = "";
+        foreach ($productos as $index => $producto) {
+            $idProducto = $producto->getIdProducto();
+            $preciounit = $producto->getPreciounit();
+            $cantidad = $producto->getCantidad();
+            $productosValues = `(@idpresupuesto, $idProducto, $preciounit, $cantidad)`;
+        }
+        $stmt = $this->db->getConnection()->prepare(`INSERT INTO presupuestos (idcliente, nrocomprobante, tipo, estado, fecha, puntoventa, total) 
+                                                     VALUES (:idcliente, :nrocomprobante, :tipo, :estado, :fecha, :puntoventa, :total);
+                                                     SET @idpresupuesto = LAST_INSERT_ID();
+                                                     INSERT INTO presupuestos (idpresupuesto, idproducto, preciounit, cantidad) VALUES `.$productosValues);
+
+        $idCliente = $presupuesto->getIdCliente();
+        $nroComprobante = $presupuesto->getNroComprobante();
+        $tipo = $presupuesto->getTipo();
+        $estado = $presupuesto->getEstado();
+        $fecha = date("d-m-Y");
+        $puntoVenta = $presupuesto->getPuntoVenta();
+        $total = $presupuesto->getTotal();
+
+        $stmt->bindParam(":idcliente", $idCliente, PDO::PARAM_INT);
+		$stmt->bindParam(":nrocomprobante", $nroComprobante, PDO::PARAM_STR);
+        $stmt->bindParam(":tipo", $tipo, PDO::PARAM_STR);
+		$stmt->bindParam(":estado", $estado, PDO::PARAM_STR);
+		$stmt->bindParam(":fecha", $fecha, PDO::PARAM_STR);
+        $stmt->bindParam(":puntoventa", $puntoVenta, PDO::PARAM_STR);
+        $stmt->bindParam(":total", $total, PDO::PARAM_STR_CHAR);
 
 		if($stmt->execute()){
 
@@ -38,10 +53,10 @@ class PresupuestoDAO {
     }
 
     public function updatePresupuesto(PresupuestoMdl $presupuesto) {
-        $stmt = $this->db->getConnection()->prepare("UPDATE presupuestos SET idcliente=:idcliente, 
+        $stmt = $this->db->getConnection()->prepare("UPDATE presupuestos SET idcliente=:idcliente,
         nrocomprobante=:nrocomprobante, estado=:estado, fecha=:fecha, puntoventa=:puntoventa,
          total=:total WHERE idpresupuesto= :idpresupuesto");
-        
+
         $idCliente = $presupuesto->getIdCliente();
         $nroComprobante = $presupuesto->getNrocomprobante();
         $estado = $presupuesto->getEstado();
@@ -49,7 +64,7 @@ class PresupuestoDAO {
         $puntoVenta = $presupuesto->getPuntoVenta();
         $total = $presupuesto->getTotal();
         $idPresupuesto = $presupuesto->getIdPresupuesto();
-        
+
 		$stmt->bindParam(":idcliente", $idCliente, PDO::PARAM_INT);
 		$stmt->bindParam(":nrocomprobante", $nroComprobante, PDO::PARAM_INT);
         $stmt->bindParam(":estado", $estado, PDO::PARAM_STR);
