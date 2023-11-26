81e377c4e9f532de07fd99e8ee641da7b99bc8cd
Ezekl22 <leonardo22eze@gmail.com>
2023-11-26 13:00:24 -0300

    feature/presupuesto
    algunos cambios para controlar si el array de presupuestos esta vacio o devuelve null

--- vistas/presupuestos/index.php
+++ vistas/presupuestos/index.php
@@ -11,82 +11,86 @@
                 </h2>
         </article>
         <article class="mt-5 d-flex flex-column align-items-center">
-            <div class="grilla contenedor__mayor-grilla d-flex flex-column align-items-center rounded-4">
-                <div class="d-flex flex-row contenedor__mayor align-items-start mt-4">
-                    <div class="d-flex w-100 alig-items-end">
-                        <div class="form-check ms-3 text__white">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Facturado
-                            </label>
+            <?php if($presupuestos && count($presupuestos))  { ?>
+                <div class="grilla contenedor__mayor-grilla d-flex flex-column align-items-center rounded-4">
+                    <div class="d-flex flex-row contenedor__mayor align-items-start mt-4">
+                        <div class="d-flex w-100 alig-items-end">
+                            <div class="form-check ms-3 text__white">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    Facturado
+                                </label>
+                            </div>
+                            <div class="form-check ms-4 text__white">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    No Facturado
+                                </label>
+                            </div>
                         </div>
-                        <div class="form-check ms-4 text__white">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="form-check-label" for="flexCheckDefault">
-                                No Facturado
-                            </label>
+                        <div class="d-flex w-100 justify-content-center">
+                            <div class="input-group w-75 input-group-sm">
+                                <label class="input-group-text input-group-sm" for="inputGroupSelect01">Tipo</label>
+                                <select class="form-select input-group-sm" id="inputGroupSelect01">
+                                    <option selected value="0">Todos</option>
+                                    <option value="1">Ventas</option>
+                                    <option value="2">Reparaciones</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="d-flex w-100 justify-content-end">
+                            <div class="input-group input-group-sm w-75">
+                                <input type="text" class="form-control" placeholder="Ingrese su busqueda" aria-label="Recipient's username" aria-describedby="buscar">
+                                <input class="btn btn-outline-secondary button" type="button" id="buscar" value="Buscar"></button>
+                            </div>
                         </div>
                     </div>
-                    <div class="d-flex w-100 justify-content-center">
-                        <div class="input-group w-75 input-group-sm">
-                            <label class="input-group-text input-group-sm" for="inputGroupSelect01">Tipo</label>
-                            <select class="form-select input-group-sm" id="inputGroupSelect01">
-                                <option selected value="0">Todos</option>
-                                <option value="1">Ventas</option>
-                                <option value="2">Reparaciones</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="d-flex w-100 justify-content-end">
-                        <div class="input-group input-group-sm w-75">
-                            <input type="text" class="form-control" placeholder="Ingrese su busqueda" aria-label="Recipient's username" aria-describedby="buscar">
-                            <input class="btn btn-outline-secondary button" type="button" id="buscar" value="Buscar"></button>
-                        </div>
-                    </div>
-                </div>
-                <div class="border contenedor__mayor mt-3 mb-5 rounded-4">
-                    <table class="grilla__contenedor border-0">
-                        <tr class="grilla grilla__cabecera">
-                            <th>Cliente</th>
-                            <th>Comprobante</th>
-                            <th>Tipo</th>
-                            <th>Estado</th>
-                            <th>Fecha</th>
-                            <th>Punto de venta</th>
-                            <th>Total</th>
-                            <th>Acciones</th>
-                        </tr>
-                        <?php foreach ($presupuestos as $presupuesto) { ?>
-                            <tr class="grilla__cuerpo">
-                                <td><?php echo $presupuestoCtr->getNombreClienteById($presupuesto['idcliente']); ?></td>
-                                <td><?php echo $presupuesto['nrocomprobante']; ?></td>
-                                <td><?php echo $presupuesto['tipo']; ?></td>
-                                <td><?php echo $presupuesto['estado']; ?></td>
-                                <td><?php echo $presupuesto['fecha']; ?></td>
-                                <td><?php echo $presupuesto['puntoventa']; ?></td>
-                                <td><?php echo '$'.number_format($presupuesto['total'], 2); ?></td>
-                                <td>
-                                    <a class="icono__contenedor me-2 ms-2" title="Cambiar estado" href="index.php?module=presupuestos&action=cambiarestado&id=<?php echo $presupuesto['idpresupuesto']; ?>">
-                                        <img class="icono__imagen" src="./assets/img/iconoCambiarEstado.svg" alt="icono de cambiar estado">
-                                    </a>
-                                    <a class="icono__contenedor me-2" title="ver" href="index.php?module=presupuestos&action=see&id=<?php echo $presupuesto['idpresupuesto']; ?>">
-                                        <img class="icono__imagen" src="./assets/img/iconoVer.png" alt="icono de ver">
-                                    </a>
-                                    <a class="icono__contenedor me-2" title="Facturar" href="index.php?module=presupuestos&action=facturar&id=<?php echo $presupuesto['idpresupuesto']; ?>">
-                                        <img class="icono__imagen" src="./assets/img/iconoFacturar.svg" alt="icono de Facturar">
-                                    </a>
-                                    <a class="icono__contenedor me-2" title="Editar" href="index.php?module=presupuestos&action=edit&id=<?php echo $presupuesto['idpresupuesto']; ?>">
-                                        <img class="icono__imagen" src="./assets/img/iconoEditar.png" alt="icono de editar">
-                                    </a>
-                                    <a class="icono__contenedor me-2" title="Cancelar" href="index.php?module=presupuestos&action=delete&id=<?php echo $presupuesto['idpresupuesto']; ?>">
-                                        <img class="icono__imagen" src="./assets/img/iconoCancelar.png" alt="icono de cancelar">
-                                    </a>
-                                </td>
+                    <div class="border contenedor__mayor mt-3 mb-5 rounded-4">
+                        <table class="grilla__contenedor border-0">
+                            <tr class="grilla grilla__cabecera">
+                                <th>Cliente</th>
+                                <th>Comprobante</th>
+                                <th>Tipo</th>
+                                <th>Estado</th>
+                                <th>Fecha</th>
+                                <th>Punto de venta</th>
+                                <th>Total</th>
+                                <th>Acciones</th>
                             </tr>
-                        <?php } ?>
-                    </table>
+                            <?php foreach ($presupuestos as $presupuesto) { ?>
+                                <tr class="grilla__cuerpo">
+                                    <td><?php echo $presupuestoCtr->getNombreClienteById($presupuesto['idcliente']); ?></td>
+                                    <td><?php echo $presupuesto['nrocomprobante']; ?></td>
+                                    <td><?php echo $presupuesto['tipo']; ?></td>
+                                    <td><?php echo $presupuesto['estado']; ?></td>
+                                    <td><?php echo $presupuesto['fecha']; ?></td>
+                                    <td><?php echo $presupuesto['puntoventa']; ?></td>
+                                    <td><?php echo '$'.number_format($presupuesto['total'], 2); ?></td>
+                                    <td>
+                                        <a class="icono__contenedor me-2 ms-2" title="Cambiar estado" href="index.php?module=presupuestos&action=cambiarestado&id=<?php echo $presupuesto['idpresupuesto']; ?>">
+                                            <img class="icono__imagen" src="./assets/img/iconoCambiarEstado.svg" alt="icono de cambiar estado">
+                                        </a>
+                                        <a class="icono__contenedor me-2" title="ver" href="index.php?module=presupuestos&action=see&id=<?php echo $presupuesto['idpresupuesto']; ?>">
+                                            <img class="icono__imagen" src="./assets/img/iconoVer.png" alt="icono de ver">
+                                        </a>
+                                        <a class="icono__contenedor me-2" title="Facturar" href="index.php?module=presupuestos&action=facturar&id=<?php echo $presupuesto['idpresupuesto']; ?>">
+                                            <img class="icono__imagen" src="./assets/img/iconoFacturar.svg" alt="icono de Facturar">
+                                        </a>
+                                        <a class="icono__contenedor me-2" title="Editar" href="index.php?module=presupuestos&action=edit&id=<?php echo $presupuesto['idpresupuesto']; ?>">
+                                            <img class="icono__imagen" src="./assets/img/iconoEditar.png" alt="icono de editar">
+                                        </a>
+                                        <a class="icono__contenedor me-2" title="Cancelar" href="index.php?module=presupuestos&action=delete&id=<?php echo $presupuesto['idpresupuesto']; ?>">
+                                            <img class="icono__imagen" src="./assets/img/iconoCancelar.png" alt="icono de cancelar">
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </table>
+                    </div>
                 </div>
-            </div>
+            <?php }else {?>
+                <h3>No hay presupuestos activos cree un presupuesto nuevo</h3>
+            <?php }?>
             <a class="my-5 btn button" type="button" href="index.php?module=presupuestos&action=create">Crear nuevo presupuesto</a>
         </article>
         <!-------------------------------------------------- Pop up ver presupuesto ---------------------------------------------->
