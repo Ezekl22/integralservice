6cf1d5866a3639abc704c9ab2c9b5752b574bdfb
Ezekl22 <leonardo22eze@gmail.com>
2023-11-26 22:29:48 -0300

    feature/presupuesto
    agrego los cambios para que se deshabiliten los botones de la grilla de facturar, editar y cambiar de estado si ya esta facturado el presupuesto

--- vistas/presupuestos/index.php
+++ vistas/presupuestos/index.php
@@ -68,20 +68,27 @@
                                     <td><?php echo '$'.number_format($presupuesto['total'], 2); ?></td>
                                     <td>
                                         <?php if($presupuesto['estado'] != 'Facturado') {?>
-                                        <a class="icono__contenedor me-2 ms-2" title="Cambiar estado" href="index.php?module=presupuestos&action=cambiarestado&id=<?php echo $presupuesto['idpresupuesto']; ?>">
-                                            <img class="icono__imagen" src="./assets/img/iconoCambiarEstado.svg" alt="icono de cambiar estado">
-                                        </a>
+                                            <a class="icono__contenedor me-2 ms-2" title="Cambiar estado" href="index.php?module=presupuestos&action=cambiarestado&id=<?php echo $presupuesto['idpresupuesto']; ?>">
+                                                <img class="icono__imagen" src="./assets/img/iconoCambiarEstado.svg" alt="icono de cambiar estado">
+                                            </a>
                                         <?php } else {?>
                                             <label class="icono__contenedor me-2 ms-2" title="Cambiar estado">
-                                                <img class="icono__imagen svg-disabled-color" src="./assets/img/iconoCambiarEstado.svg" alt="icono de cambiar estado">
+                                                <img class="icono__imagen svg-disabled-color" src="./assets/img/iconoCambiarEstadoDeshabilitado.svg" alt="icono de cambiar estado">
                                             </label>
                                         <?php }?>
                                         <a class="icono__contenedor me-2" title="ver" href="index.php?module=presupuestos&action=see&id=<?php echo $presupuesto['idpresupuesto']; ?>">
                                             <img class="icono__imagen" src="./assets/img/iconoVer.png" alt="icono de ver">
                                         </a>
-                                        <a class="icono__contenedor me-2" title="Facturar" href="index.php?module=presupuestos&action=facturar&id=<?php echo $presupuesto['idpresupuesto']; ?>">
-                                            <img class="icono__imagen" src="./assets/img/iconoFacturar.svg" alt="icono de Facturar">
-                                        </a>
+                                        <?php if($presupuesto['estado'] != 'Facturado') {?>
+                                            <a class="icono__contenedor me-2" title="Facturar" href="index.php?module=presupuestos&action=facturar&id=<?php echo $presupuesto['idpresupuesto']; ?>">
+                                                <img class="icono__imagen" src="./assets/img/iconoFacturar.svg" alt="icono de Facturar">
+                                            </a>
+                                        <?php } else {?>
+                                            <label class="icono__contenedor me-2" title="Facturar">
+                                                <img class="icono__imagen" src="./assets/img/iconoFacturarDeshabilitado.svg" alt="icono de Facturar">
+                                            </label>
+                                        <?php }?>
+                                        
                                         <?php if($presupuesto['estado'] != 'Facturado') {?>
                                             <a class="icono__contenedor me-2" title="Editar" href="index.php?module=presupuestos&action=edit&id=<?php echo $presupuesto['idpresupuesto']; ?>">
                                                 <img class="icono__imagen" src="./assets/img/iconoEditar.png" alt="icono de editar">
