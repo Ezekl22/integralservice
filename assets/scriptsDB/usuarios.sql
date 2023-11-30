0b1095d668ba6fc68d0620fde9b51cd21bfdea50
Ezekl22 <leonardo22eze@gmail.com>
2023-11-29 23:02:15 -0300

    main
    hago cambos en los scripts

--- assets/scriptsDB/usuarios.sql
+++ assets/scriptsDB/usuarios.sql
@@ -1,14 +1,15 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `lastname` text NOT NULL,
-  `type` text NOT NULL,
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE `usuarios` (
+  `idusuario` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `apellido` text NOT NULL,
+  `tipo` text NOT NULL,
   `mail` text NOT NULL,
-  `password` text NOT NULL
+  `constrasena` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `users` (`id`, `name`, `lastname`, `type`, `mail`, `password`) VALUES
+INSERT INTO `users` (`idusuario`, `nombre`, `apellido`, `tipo`, `mail`, `contrasena`) VALUES
 (1, 'Pedro1', 'Pintoss', 'Administrador', 'Pedro22@gmail.com', '123'),
 (2, 'ADMINISTRADOR', 'ADMIN', 'ADMINISTRADOR', 'ADMINISTRADOR@gmail.com', '123'),
 (3, 'Alberto', 'Centurion', 'REPARADOR', 'Alberto22@gmail.com', '123');
