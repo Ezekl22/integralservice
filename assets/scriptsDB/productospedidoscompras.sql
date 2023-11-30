717edb8179b59ea8ce68e3d2a509474eabfb2647
Ezekl22 <leonardo22eze@gmail.com>
2023-11-29 21:25:47 -0300

    feature/pedidoCompra
    pequeño cambio en el script

--- assets/scriptsDB/productospedidoscompras.sql
+++ assets/scriptsDB/productospedidoscompras.sql
@@ -15,12 +15,13 @@ INSERT INTO `productospedidoscompras` (`idproductopedidocompra`, `idpedidocompra
 
 ALTER TABLE `productospedidoscompras`
   ADD PRIMARY KEY (`idproductopedidocompra`),
-  ADD UNIQUE KEY `idpedidocompra` (`idpedidocompra`,`idproducto`),
+  ADD KEY `idpedidocompra` (`idpedidocompra`,`idproducto`),
   ADD KEY `idproducto` (`idproducto`);
 
 ALTER TABLE `productospedidoscompras`
   MODIFY `idproductopedidocompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 ALTER TABLE `productospedidoscompras`
-  ADD CONSTRAINT `productospedidoscompras_ibfk_1` FOREIGN KEY (`idpedidocompra`) REFERENCES `presupuestos` (`idpedidocompra`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `productospedidoscompras_ibfk_1` FOREIGN KEY (`idpedidocompra`) REFERENCES `pedidoscompras` (`idpedidocompra`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `productospedidoscompras_ibfk_2` FOREIGN KEY (`idproducto`) REFERENCES `productos` (`idproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
