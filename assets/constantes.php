cc94c0689a5d0b9bf2b9c608f9512c495a79dcaf
Ezekl22 <leonardo22eze@gmail.com>
2023-12-20 20:25:53 -0300

    feature/usuario

--- assets/constantes.php
+++ assets/constantes.php
@@ -34,8 +34,8 @@ $GrillaUsuariosIndex = array("Nombre","Apellido","Tipo","Mail");
 
 
 // Datos del popup de inicio de sesion
-$inicioSesionCuerpoP = '<input id="mail" type="text" class="mb-4 mx-5 form-control w-75" name="mail" placeholder="Mail">
-                        <input type="text" id="contrasena"  name="contrasena" class="mx-5 form-control w-75" placeholder="Contraseña">
+$inicioSesionCuerpoP = '<input id="mail" type="text" class="mb-4 mx-5 form-control w-75" name="mail" placeholder="Mail" required>
+                        <input type="text" id="contrasena"  name="contrasena" class="mx-5 form-control w-75" placeholder="Contraseña" required>
                         <div class="d-flex justify-content-center">
                             <button type="button" class="btn btn-link btn__recuperarC" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#recuperarCon">
                                     Recuperar contraseña
@@ -47,21 +47,22 @@ $InicioSesionBotonesP = array(["texto" => "Cancelar","tipo" => "button","href" =
 // Datos del popup de recuperar contraseña
 $recuperarContrasenaCuerpoP = '<form action="index.php?action=login" method="POST" class=" d-flex flex-column align-items-center">
                                     <div class="d-flex mb-4 mx-5">Ingrese su correo electrónico y recibirá un código de verificación.</div>
-                                    <input type="text" class="mb-4 mx-5 form-control w-75" placeholder="mail">
+                                    <input type="text" class="mb-4 mx-5 form-control w-75" placeholder="mail" required>
                                 </form>';
 
 $recuperarContrasenaBotonesP = array(["texto" => "cancelar","tipo" => "button","href" => ""],["texto" => "Enviar","tipo" => "submit","href" => ""]);
 
 // Datos del popup de inicio de ingresar codigo
 $ingresarCodigoCuerpoP = '<div class="d-flex mb-4 mx-5">Ingrese el codigo que se envio a su correo.</div>
-                          <input type="text" class="mx-5 mb-4" placeholder="Código">';
+                          <input type="text" class="mx-5 mb-4" placeholder="Código" required>';
 
 $ingresarCodigoBotonesP = array(["texto" => "cancelar","tipo" => "button","href" => ""],["texto" => "Verificar","tipo" => "submit","href" => ""]);
 
 // Datos del popup de inicio de ingresar codigo
 $cambiarContrasenaCuerpoP = '<form action="index.php?action=login" method="POST" class=" d-flex flex-column align-items-center">
-                                <div class="d-flex mb-4 mx-5">Ingrese el codigo que se envio a su correo.</div>
-                                <input type="text" class="mx-5 mb-4" placeholder="Código">
+                                <div class="d-flex mb-4 mx-5">Ingrese la nueva contraseña.</div>
+                                <input type="text" class="mx-5 mb-4" placeholder="Nueva contraseña" required>
+                                <input type="text" class="mx-5 mb-4" placeholder="Repita la contraseña" required>
                             </form>';
 
 $cambiarContrasenaBotonesP = array(["texto" => "cancelar","tipo" => "button","href" => ""],["texto" => "Guardar","tipo" => "submit","href" => ""]);
