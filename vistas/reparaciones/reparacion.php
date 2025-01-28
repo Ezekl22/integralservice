161c662ceefa018051442518b97b5a6372ed8d3a
Ezekl22 <leonardo22eze@gmail.com>
2025-01-28 01:46:56 -0300

    feature/reaparacion
    agrego estos cambios para que funcione el buscador

--- vistas/reparaciones/reparacion.php
+++ vistas/reparaciones/reparacion.php
@@ -14,13 +14,16 @@
         </article>
         <article class="mt-5 d-flex flex-column align-items-center">
             <div class="grilla w-95 d-flex flex-column align-items-center rounded-4">
-                <div class="d-flex w-75 justify-content-end mt-3">
-                    <div class="input-group input-group-sm w-25">
-                        <input type="text" class="form-control" placeholder="Ingrese su busqueda"
+                <div class="d-flex w-95 justify-content-end mt-3">
+                    <form action="index.php?module=presupuestos&action=searched" method="POST"
+                        class="input-group input-group-sm w-25" id="formBuscador">
+                        <input type="search" id="termino" name="termino" class="form-control"
+                            placeholder="Ingrese su busqueda"
+                            value="<?php echo isset($_POST['termino']) ? $_POST['termino'] : ""; ?>"
                             aria-label="Recipient's username" aria-describedby="buscar">
-                        <input class="btn btn-outline-secondary button" type="button" id="buscar"
+                        <input class="btn btn-outline-secondary button" type="submit" id="buscar"
                             value="Buscar"></button>
-                    </div>
+                    </form>
                 </div>
                 <?php $grillaCtr->mostrarGrilla(); ?>
             </div>
